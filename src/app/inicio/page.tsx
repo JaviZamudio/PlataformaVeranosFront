f5a1b5b0af3b2c1392475c7055ad2f6bce6b2f41
roub <fernandoroub@gmail.com>
2023-10-04 13:36:29 -0600

    Filtros hechos

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -12,12 +12,12 @@ export default function Inicio() {
       <Header />
 
       <main>
-        <section> 
+        <section className='flex items-center gap-28 p-[80px]'> 
           
           <Filtros/>
 
         </section>
-        <section className='p-20 grid grid-cols-3 gap-x-28 gap-y-10 content-start'>   
+        <section className='px-20 grid grid-cols-3 gap-x-28 gap-y-10 content-start'>   
       
           <Tarjeta/>
 
--- src/components/filtros.tsx
+++ src/components/filtros.tsx
@@ -1,42 +1,87 @@
 import React from 'react';
 import { Select, Option, Input, Button } from "@material-tailwind/react";
-   
-  export function Filtros () {
+
+export function Filtros() {
 
     return (
         <>
-            <div className="flex w-72 flex-col gap-6">
-            
-                <Select variant="outlined" label="Carrera">
-                <Option>1</Option>
-                <Option>2</Option>
-                <Option>3</Option>
-                <Option>4</Option>
-                <Option>5</Option>
-                </Select>
-                <Select variant="outlined" label="Semestre">
-                <Option>1er Semestre</Option>
-                <Option>2do Semestre</Option>
-                <Option>3er Semestre</Option>
-                <Option>4to Semestre</Option>
-                <Option>5to Semestre</Option>
-                <Option>6to Semestre</Option>
-                <Option>7mo Semestre</Option>
-                <Option>8vo Semestre</Option>
+            <div className='w-72'>
+                <Select variant="outlined" label="Carrera" className='bg-[#d9dfef]'>
+                    <Option>
+                        <img src="/images/info.png" alt="" className='inline-block' />
+                        <p className='inline-block'>
+                            Licenciatura en informática
+                        </p>
+                    </Option>
+                    <Option>
+                        <img src="/images/lati.png" alt="" className='inline-block' />
+                        <p className='inline-block'>
+                            Licenciatura en Administración de las T.I.
+                        </p>
+                    </Option>
+                    <Option>
+                        <img src="/images/soft.png" alt="" className='inline-block' />
+                        <p className='inline-block'>
+                            Ingeniería de Software
+                        </p>
+                    </Option>
+                    <Option>
+                        <img src="/images/compu.png" alt="" className='inline-block' />
+                        <p className='inline-block'>
+                            Ingeniería en Computación
+                        </p>
+                    </Option>
+                    <Option>
+                        <img src="/images/tele.png" alt="" className='inline-block' />
+                        <p className='inline-block'>
+                            Ingeniería en Telecomunicaciones y Redes
+                        </p>
+                    </Option>
+                    <Option>
+                        <img src="/images/icad.png" alt="" className='inline-block' />
+                        <p className='inline-block'>
+                            Ingeniería en Ciencia y Analítica de Datos
+                        </p>
+                    </Option>
+                    <Option>
+                        <img src="/images/itic22_1.png" alt="" className='inline-block' />
+                        <p className='inline-block'>
+                            Ingeniería en Tecnologías de Información y Ciberseguridad
+                        </p>
+                    </Option>
                 </Select>
             </div>
-            <div className="relative flex w-full max-w-[24rem]">
-                <Input 
-                    label='Username' 
-                    crossOrigin={undefined}                >
-                </Input>
+
+            <div className='w-72'>
+                <Select variant="outlined" label="Semestre" className='bg-[#d9dfef]'>
+                    <Option>1er Semestre</Option>
+                    <Option>2do Semestre</Option>
+                    <Option>3er Semestre</Option>
+                    <Option>4to Semestre</Option>
+                    <Option>5to Semestre</Option>
+                    <Option>6to Semestre</Option>
+                    <Option>7mo Semestre</Option>
+                    <Option>8vo Semestre</Option>
+                </Select>
+            </div>
+
+            <div className="relative flex w-full max-w-[28rem]">
+                <Input
+                    type='search'
+                    label='Nombre o clave de la materia'
+                    crossOrigin={undefined}
+                    className='rounded-3xl bg-[#d9dfef]'
+                />
+
                 <Button
                     size="sm"
-                    className="!absolute right-1 top-1 rounded"
+                    className="absolute right-0 h-full rounded-l-[0px] rounded-r-3xl bg-azul-oscuro"
                 >
-                    Invite
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    </svg>
                 </Button>
             </div>
         </>
     );
-  }
+}
