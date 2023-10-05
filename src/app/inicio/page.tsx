422ded37a52a45cbc8a61dda23663844662456d4
JaviZamudio <javier.elihu100@gmail.com>
2023-10-05 12:36:49 -0600

    useEffect y useState metidos dentro de componente

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -5,46 +5,46 @@ import Header from '@/components/Header';
 import Tarjeta from '@/components/tarjetaMateria';
 import { Filtros } from '@/components/filtros';
 
-const [data, setData] = useState([]);
+
+export default function Inicio() {
+  const [data, setData] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await fetch('/ruta');
+      if (!response.ok) {
+        throw new Error('La solicitud no fue exitosa.');
+      }
+      const responseData = await response.json();
+      setData(responseData);
+    } catch (error) {
+      console.error('Error en la solicitud: ', error);
+    }
+  }
 
   useEffect(() => {
-
-    async function fetchData() {
-      try {
-        const response = await fetch('/ruta'); 
-        if (!response.ok) {
-          throw new Error('La solicitud no fue exitosa.');
-        }
-        const responseData = await response.json();
-        setData(responseData);
-      } catch (error) {
-        console.error('Error en la solicitud: ', error);
-      }
-    }
-
     fetchData();
   }, []);
 
-export default function Inicio() {
   return (
     <>
       <Header />
 
       <main>
-        <section className='flex items-center gap-28 p-[80px]'> 
-          
-          <Filtros/>
+        <section className='flex items-center gap-28 p-[80px]'>
+
+          <Filtros />
 
         </section>
-        <section className='px-20 grid grid-cols-3 gap-x-28 gap-y-10 content-start'>   
-      
-          <Tarjeta/>
+        <section className='px-20 grid grid-cols-3 gap-x-28 gap-y-10 content-start'>
 
-          <Tarjeta/>
+          <Tarjeta />
 
-          <Tarjeta/>
+          <Tarjeta />
 
-          <Tarjeta/>
+          <Tarjeta />
+
+          <Tarjeta />
 
         </section>
       </main>
