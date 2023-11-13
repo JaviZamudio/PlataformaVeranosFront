234d65b291f4b521a7ff9288fc2d1262d4188aaa
roub2501 <fernandoroub@gmail.com>
2023-11-13 14:42:53 -0600

    Campo de inscritos agregado a la tarjeta

--- src/components/tarjetaMateriaAdmin.tsx
+++ src/components/tarjetaMateriaAdmin.tsx
@@ -198,6 +198,16 @@ export default function TarjetaAdmin() {
                 $350
               </Typography>
             </div>
+
+            <div className='flex absolute right-4 bottom-2'>
+              <Typography variant="paragraph" color="white" className="mt-2 mr-1 font-semibold">
+                Inscritos:
+              </Typography>
+              <Typography variant="paragraph" color="white" className="mt-2 font-normal">
+                8
+              </Typography>
+            </div>
+
           </div>
         </CardBody>
 
@@ -211,7 +221,7 @@ export default function TarjetaAdmin() {
 
             <p className=''>Información</p>
           </Button> */}
-          <Button variant='text' className='flex items-center text-white w-1/2'>
+          <Button variant='text' className='flex place-items-center text-white w-1/2'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
               <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
             </svg>
