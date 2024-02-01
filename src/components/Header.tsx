0c2b4291ffb5297c8e928cacdf9b8c50f4623f72
JaviZamudio <javier.elihu100@gmail.com>
2024-02-01 10:10:52 -0600

    corrected header and general layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
 import { Button } from "@material-tailwind/react";
+import Image from 'next/image';
 
 export default function Header() {
-    return (
-
-        <header className="relative w-full h-36 bg-[url('/images/fondoHeader.png')]">
-            <img src="images/logoHeader.png" alt="" className='float-left h-[100px] w-auto' />
-            <nav className='absolute w-full h-10 bottom-0 bg-azul-marino'>
-             <ul className='float-right h-10 items-center'>
-                <li className='relative inline-block'>
-                <Button variant="text" className='rounded-none text-white'>  
-                    <Link href="/inicio">
-                    Inicio
-                    </Link>
-                </Button>
-                </li>
-                <li className='relative inline-block'>
-                <Button variant="text" className='rounded-none text-white'>
-                    <Link href="/solicitud">
-                    Solicitar materia
-                    </Link>
-                </Button>
-                </li>
-            </ul> 
-            </nav>
-        </header>
-    )
+  return (
+    <header className="flex flex-col w-full">
+      <div className='relative w-full h-24'>
+        <Image src="/images/fondoHeader.png" alt="hero fondo" layout='fill' className='w-full h-24 object-cover object-top' />
+        <Image src="/images/logoHeader.png" alt="hero" className='h-24 w-auto absolute' width={1000} height={100} />
+      </div>
+      <nav className='w-full h-10 bg-azul-marino flex justify-end'>
+        <ul className='h-full items-center'>
+          <li className='inline-block'>
+            <Link href="/inicio">
+              <Button variant="text" className='rounded-none text-white'>
+                Inicio
+              </Button>
+            </Link>
+          </li>
+          <li className='inline-block'>
+            <Link href="/solicitud">
+              <Button variant="text" className='rounded-none text-white'>
+                Solicitar materia
+              </Button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
