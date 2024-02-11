af5350806aa7c0fb72b4c0809248ecb2c642bacd
JaviZamudio <javier.elihu100@gmail.com>
2024-02-11 02:11:24 -0600

    Improved admin feed.

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import React, { useContext, useEffect, useState } from 'react';
-import TarjetaAdmin from '@/components/tarjetaMateriaAdmin';
-import AdminHeader from '@/components/HeaderAdmin'
-import { Filtros } from '@/components/filtros'
 import NewFiltros from '@/components/NewFiltros';
 import { HOST } from '@/configs';
-import NewTarjetaAdmin from '@/components/NewTarjetaAdmin';
 import { AuthContext } from '@/contexts/AuthContext';
+import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from "@nextui-org/table";
+import { Button } from '@nextui-org/button';
+import GroupInfoModal from '@/components/GroupInfoModal';
+import Link from 'next/link';
 
 interface GroupData {
   id_grupo: string;
@@ -16,23 +16,26 @@ interface GroupData {
   area: string;
   area_img: string;
   inscritos: number;
-  horario: string;
-  profesor: string;
-  costo: number;
   carreras: string[];
+  horario?: string;
+  profesor?: string;
+  costo?: number;
+}
+
+interface Materia {
+  clave: string;
+  nombre: string;
+  carreras: string[];
+  grupo?: GroupData;
 }
 
 export default function InicioAdmin() {
   const { token } = useContext(AuthContext);
   const [grupos, setGroups] = useState<GroupData[]>([]);
+  const [materias, setMaterias] = useState<Materia[]>([]);
 
   async function fetchData() {
-    if (!token) {
-      console.log('No hay token');
-      return;
-    }
-
-    const resBody = await fetch(`${HOST}/api/admin/groups`, {
+    const resBody = await fetch(`${HOST}/api/admin/materias`, {
       method: 'GET',
       headers: {
         'Authorization': token || ''
@@ -46,22 +49,88 @@ export default function InicioAdmin() {
       return;
     }
 
-    setGroups(resBody.data);
+    setMaterias(resBody.data);
   }
 
   useEffect(() => {
-    fetchData();
+    token && fetchData();
   }, [token]);
 
   return (
     <main className="p-4 flex flex-col items-center">
       <NewFiltros />
 
-      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-6xl">
-        {grupos.map((grupo, i) => {
-          return <NewTarjetaAdmin key={i} group={grupo} />
-        })}
-      </div>
+      <Table aria-label="Example static collection table" className="w-full max-w-6xl mt-4" isStriped >
+        <TableHeader>
+          <TableColumn>NOMBRE</TableColumn>
+          <TableColumn>CLAVE</TableColumn>
+          <TableColumn>CARRERAS</TableColumn>
+          <TableColumn>GRUPO</TableColumn>
+          <TableColumn>INSCRITOS</TableColumn>
+          <TableColumn>INFORMACIÓN</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent="No hay materias para mostrar">
+          {materias.map(materia => (
+            <TableRow key={materia.clave}>
+              <TableCell>{materia.nombre}</TableCell>
+              <TableCell>{materia.clave}</TableCell>
+              <TableCell>{materia.carreras.join(", ")}</TableCell>
+              <TableCell>
+                {materia.grupo ?
+                  <GroupInfoModal group={materia.grupo}>
+                    <Button
+                      color="primary"
+                      variant="solid"
+                      size='md'
+                      onPress={() => console.log('Ver grupo', materia.grupo)}
+                      startContent={
+                        <span className="material-symbols-outlined">info</span>
+                      }
+                      className='w-36'
+                    >
+                      Ver Grupo
+                    </Button>
+                  </GroupInfoModal>
+                  :
+                  <Button
+                    color="primary"
+                    variant="flat"
+                    size='md'
+                    as={Link}
+                    href={`/admin/grupos/crear?materia=${materia.clave}`}
+                    startContent={
+                      <span className="material-symbols-outlined">add_box</span>
+                    }
+                    className='w-36'
+                  >
+                    Crear Grupo
+                  </Button>
+                }
+              </TableCell>
+              <TableCell>{materia.grupo ? materia.grupo.inscritos : "-"}</TableCell>
+              <TableCell>
+                {!materia.grupo ?
+                  ("-") :
+                  materia.grupo.profesor && materia.grupo.horario && materia.grupo.costo ?
+                    "Completa" :
+                    <Button
+                      color="primary"
+                      variant="faded"
+                      size='md'
+                      as={Link}
+                      href={`/admin/grupos/editar?materia=${materia.clave}`}
+                      startContent={
+                        <span className="material-symbols-outlined">edit</span>
+                      }
+                    >
+                      Editar
+                    </Button>
+                }
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </main >
   )
 }
---
+++ src/components/GroupInfoModal.tsx
@@ -0,0 +1,101 @@
+"use client"
+
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { ReactElement, cloneElement, useState } from "react";
+
+interface GroupData {
+  clave_materia: number;
+  nombre_materia: string;
+  area: string;
+  area_img: string;
+  inscritos: number;
+  horario?: string;
+  profesor?: string;
+  costo?: number;
+  carreras: string[];
+}
+
+export default function GroupInfoModal({ group, children }: { group: GroupData, children: ReactElement }) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const router = useRouter();
+
+  return (
+    <>
+      {/* Modal */}
+      <Modal
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        size="lg"
+      >
+        <ModalContent>
+          <Image
+            src={group.area_img || "/images/Arquitectura de las computadoras.jpg"}
+            className="h-56 w-full object-cover mb-2"
+            alt="Arquitectura de las computadoras"
+            width={500} height={200}
+          />
+          <ModalBody>
+            <h2 className="text-xl font-bold">{group.nombre_materia}</h2>
+            <Divider />
+            <div className="grid grid-cols-2 gap-2">
+              <div className="flex items-center">
+                <p className="text-base font-semibold">Area:</p>
+                <p className="text-base ml-2">{group.area}</p>
+              </div>
+              <div className="flex items-center">
+                <p className="text-base font-semibold">Carreras:</p>
+                <p className="text-base ml-2">{group.carreras?.join(", ")}</p>
+              </div>
+              <div className="flex items-center">
+                <p className="text-base font-semibold">Profesor:</p>
+                <p className="text-base ml-2">{group.profesor}</p>
+              </div>
+              <div className="flex items-center">
+                <p className="text-base font-semibold">Horario:</p>
+                <p className="text-base ml-2">{group.horario}</p>
+              </div>
+              <div className="flex items-center">
+                <p className="text-base font-semibold">Costo:</p>
+                <p className="text-base ml-2">{group.costo}</p>
+              </div>
+              <div className="flex items-center">
+                <p className="text-base font-semibold">Clave:</p>
+                <p className="text-base ml-2">{group.clave_materia}</p>
+              </div>
+              <div className="flex items-center col-span-2">
+                <p className="text-base font-semibold">Registrados:</p>
+                <p className="text-base ml-2">{`${group.inscritos}/20 para abrir el grupo`}</p>
+              </div>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color="primary"
+              variant="light"
+              onPress={() => setModalOpen(false)}
+            >
+              Cerrar
+            </Button>
+
+            <Button
+              color="primary"
+              startContent={
+                <span className="material-symbols-outlined">edit_square</span>
+              }
+              // onPress={() => router.push(`/admin/grupos/${group.id_grupo}`)}
+              as={Link}
+              href={`/admin/grupos/editar?materia=${group.clave_materia}`}
+            >
+              Editar
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      {cloneElement(children, { onPress: () => setModalOpen(true) })}
+    </>
+  )
+}
