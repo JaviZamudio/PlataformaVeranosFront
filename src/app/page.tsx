6e20470af42948237ed38039dfc6f6cf3ba9f9a1
JaviZamudio <javier.elihu100@gmail.com>
2024-03-08 23:30:10 -0600

    hard Rename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
+  // TODO: Tabla de solicitudes en materia
+  // TODO: Agregar campo de Numero Telefono
+  // TODO: Hacer el Apellido Materno opcional
   redirect('/inicio');
 }
